5e888c51db0f42da1e30ba2632523caa6fe9dd53
wyatt <grodzilla93@gmail.com>
2019-10-08 10:37:02 -0700

    almost got the scafolding done as i follow along

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,7 @@
 
 var  = document.createElement
 
-var shop shophours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm','3pm', '4pm', '5pm', '6pm', '7pm','8pm']
+var shophours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm','3pm', '4pm', '5pm', '6pm', '7pm','8pm']
 
 var ulSeattle = document.getElementById('Seattle')
 
@@ -14,9 +14,23 @@ var shopSeattle ={
     averageCookiesPerCustomer: 6.3,
     customersEachHour:[],
     cookiesEachHour: [],
-    totalCookiesPerDay: 0
-}
+		totalCookiesPerDay: 0,
+		for( var i = 0; i < shophours.length; i++) {
+		var customers = randomNumber(this.minCustomersPerhour, this.maxCustomersPerhour);
+		generateCustomersEachHour: function() {
+			this.cookiesEachHour.push(randomNumber);
+		}
+		},
 
-function randomNumGenerator(min, max) {
+		generateCookiesEachHour: function(){
+			this.generateCustomersEachHour();
+			for( var i = 0; i < hours.length; i++) {
+			var cookiesForOneHour = Math.ceil(this.customersEachHour * this.averageCookiesPerCustomer)
+			this.cookiesEachHour.push(cookiesForOneHour);
+		}
+	}
+};
+
+function randomnumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; 
   }
