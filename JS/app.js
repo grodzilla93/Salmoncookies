445387571b22dcc56b152bb45d1e144a12463760
wyatt <grodzilla93@gmail.com>
2019-10-08 15:26:09 -0700

    because i havent updated in too long

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,6 @@
 'use strict';
 
 
-
-var  = document.createElement
-
 var shophours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm','3pm', '4pm', '5pm', '6pm', '7pm','8pm']
 
 var ulSeattle = document.getElementById('Seattle')
@@ -15,21 +12,38 @@ var shopSeattle ={
     customersEachHour:[],
     cookiesEachHour: [],
 		totalCookiesPerDay: 0,
-		for( var i = 0; i < shophours.length; i++) {
-		var customers = randomNumber(this.minCustomersPerhour, this.maxCustomersPerhour);
+
 		generateCustomersEachHour: function() {
+			for( var i = 0; i < shophours.length; i++) {
+			var customers = randomNumber(this.minCustomersPerhour, this.maxCustomersPerhour);
+		
 			this.cookiesEachHour.push(randomNumber);
-		}
-		},
+			}
+			}
+		
 
 		generateCookiesEachHour: function(){
 			this.generateCustomersEachHour();
 			for( var i = 0; i < hours.length; i++) {
 			var cookiesForOneHour = Math.ceil(this.customersEachHour * this.averageCookiesPerCustomer)
 			this.cookiesEachHour.push(cookiesForOneHour);
+			this.totalCookiesPerDay += cookiesForOneHour;
 		}
-	}
+		},
+}
+render: function() {
+	for( var i = 0; i < hours.length; i++){
+		var liEL = document.createElement('li');
+		liEL.textContent = `${hours[i]} Cookies:${this.cookiesEachHour[i]}`;
+		ulSeattle.appendChild(liEL);
+}
+var liELTotal = document.createElement('li');
+liELTotal.textContent = `Daily totals: ${liELTotal}`;
+ulshopSeattle.appendChild(liELTotal);
 };
+shopSeattle.generateCustomersEachHour();
+shopSeattle.generateCookiesEachHour();
+shopSeattle.render();
 
 function randomnumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; 
