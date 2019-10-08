68b338642d6d874c4dbc3267cf94013646f006b8
wyatt <grodzilla93@gmail.com>
2019-10-08 16:34:36 -0700

    Seattle is fixed! everything works! I am 1/4th of the way there!

--- JS/app.js
+++ JS/app.js
@@ -1,50 +1,54 @@
 'use strict';
 
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
-var shophours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm','3pm', '4pm', '5pm', '6pm', '7pm','8pm']
+var shophours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm','3pm', '4pm', '5pm', '6pm', '7pm','8pm'];
 
-var ulSeattle = document.getElementById('Seattle')
+var ulSeattle = document.getElementById('Seattle');
 
 var shopSeattle ={
-    minCustomersPerhour: 23,
-    maxCustomersPerhour: 65,
-    averageCookiesPerCustomer: 6.3,
-    customersEachHour:[],
-    cookiesEachHour: [],
-		totalCookiesPerDay: 0,
+  minCustomersPerhour: 23,
+  maxCustomersPerhour: 65,
+  averageCookiesPerCustomer: 6.3,
+  customersEachHour:[],
+  cookiesEachHour: [],
+  totalCookiesPerDay: 0,
 
-		generateCustomersEachHour: function() {
-			for( var i = 0; i < shophours.length; i++) {
-			var customers = randomNumber(this.minCustomersPerhour, this.maxCustomersPerhour);
-		
-			this.cookiesEachHour.push(randomNumber);
-			}
-			}
-		
+  generateCustomersEachHour: function() {
+    for( var i = 0; i < shophours.length; i++) {
+      var customers = randomNumber(this.minCustomersPerhour, this.maxCustomersPerhour);
 
-		generateCookiesEachHour: function(){
-			this.generateCustomersEachHour();
-			for( var i = 0; i < hours.length; i++) {
-			var cookiesForOneHour = Math.ceil(this.customersEachHour * this.averageCookiesPerCustomer)
-			this.cookiesEachHour.push(cookiesForOneHour);
-			this.totalCookiesPerDay += cookiesForOneHour;
+      this.customersEachHour.push(customers);
 		}
-		},
-}
-render: function() {
-	for( var i = 0; i < hours.length; i++){
-		var liEL = document.createElement('li');
-		liEL.textContent = `${hours[i]} Cookies:${this.cookiesEachHour[i]}`;
-		ulSeattle.appendChild(liEL);
-}
-var liELTotal = document.createElement('li');
-liELTotal.textContent = `Daily totals: ${liELTotal}`;
-ulshopSeattle.appendChild(liELTotal);
+  },
+
+
+  generateCookiesEachHour: function(){
+    this.generateCustomersEachHour();
+    for( var i = 0; i < shophours.length; i++) {
+			var cookiesForOneHour = Math.ceil(this.customersEachHour[i] * this.averageCookiesPerCustomer);
+			console.log(this.customersEachHour[i])
+      this.cookiesEachHour.push(cookiesForOneHour);
+      console.log(cookiesForOneHour);
+      this.totalCookiesPerDay += cookiesForOneHour;
+    }
+  },
+  render: function() {
+    for( var i = 0; i < shophours.length; i++){
+      var liEL = document.createElement('li');
+      liEL.textContent = `${shophours[i]} Cookies:${this.cookiesEachHour[i]}`;
+      ulSeattle.appendChild(liEL);
+    }
+  }
 };
-shopSeattle.generateCustomersEachHour();
+
+// shopSeattle.generateCustomersEachHour();
 shopSeattle.generateCookiesEachHour();
 shopSeattle.render();
 
-function randomnumber(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min; 
-  }
+
+var liELTotal = document.createElement('li');
+liELTotal.textContent = `Daily totals: ${shopSeattle.totalCookiesPerDay}`;
+ulSeattle.appendChild(liELTotal);
