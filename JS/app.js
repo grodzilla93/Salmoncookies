ef0d1be4e3469dfe6b97656d8d0b68aa2ac3c24a
wyatt <grodzilla93@gmail.com>
2019-10-08 21:00:46 -0700

    giving up on lab06, too much time an energy being spent to make a square wheel

--- JS/app.js
+++ JS/app.js
@@ -6,30 +6,51 @@ function randomNumber(min, max) {
 
 var shophours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm','3pm', '4pm', '5pm', '6pm', '7pm','8pm'];
 
+
 var ulSeattle = document.getElementById('Seattle');
 
-var shopSeattle ={
-  minCustomersPerhour: 23,
-  maxCustomersPerhour: 65,
-  averageCookiesPerCustomer: 6.3,
-  customersEachHour:[],
-  cookiesEachHour: [],
-  totalCookiesPerDay: 0,
+function salesPerCity(location, minCustomersPerhour, maxCustomersPerhour,averageCookiesPerCustomer){
+  this.location = location;
+  this.minCustomersPerhour = minCustomersPerhour;
+  this.maxCustomersPerhour = maxCustomersPerhour;
+  this.averageCookiesPerCustomer = averageCookiesPerCustomer;
+  this.salesPerCity = [];
+  salesPerCity.call.push(this);
+}
+
+
+// var shopSeattle ={
+//   minCustomersPerhour: 23,
+//   maxCustomersPerhour: 65,
+//   averageCookiesPerCustomer: 6.3,
+//   customersEachHour:[],
+//   cookiesEachHour: [],
+//   totalCookiesPerDay: 0,
+
+var seattle = new SalesPerLocation('Seattle', 23, 65, 6.3);
+
+var tokyo = new SalesPerLocation('Tokyo', 3, 24, 1.2);
+
+var dubai = new SalesPerLocation('Dubai', 11, 38, 3.7);
+
+var paris = new SalesPerLocation('Paris', 20, 38, 2.3);
+
+var lima = new SalesPerLocation('Lima', 2, 16, 4.6);
 
   generateCustomersEachHour: function() {
     for( var i = 0; i < shophours.length; i++) {
       var customers = randomNumber(this.minCustomersPerhour, this.maxCustomersPerhour);
 
       this.customersEachHour.push(customers);
-		}
+    }
   },
 
 
   generateCookiesEachHour: function(){
     this.generateCustomersEachHour();
     for( var i = 0; i < shophours.length; i++) {
-			var cookiesForOneHour = Math.ceil(this.customersEachHour[i] * this.averageCookiesPerCustomer);
-			console.log(this.customersEachHour[i])
+      var cookiesForOneHour = Math.ceil(this.customersEachHour[i] * this.averageCookiesPerCustomer);
+      console.log(this.customersEachHour[i]);
       this.cookiesEachHour.push(cookiesForOneHour);
       console.log(cookiesForOneHour);
       this.totalCookiesPerDay += cookiesForOneHour;
@@ -45,8 +66,8 @@ var shopSeattle ={
 };
 
 // shopSeattle.generateCustomersEachHour();
-shopSeattle.generateCookiesEachHour();
-shopSeattle.render();
+// shopSeattle.generateCookiesEachHour();
+// shopSeattle.render();
 
 
 var liELTotal = document.createElement('li');
